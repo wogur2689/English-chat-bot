6321e305e315da243888d23747ef844b87b90e51
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-11-20 15:56:16 +0900

    initial commit

---
+++ src/main/java/com/example/englishchatbot/EnglishChatBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.englishchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishChatBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishChatBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/englishchatbot/EnglishChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.englishchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishChatBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
